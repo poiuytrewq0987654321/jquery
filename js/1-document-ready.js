3256df19834a4f256caac3bac075d620e724df0f
poiuytrewq0987654321 <bundushathurh@gmail.com>
2017-06-24 14:31:28 +0200

    jquery - cwiczenia
    document ready
    selektory

---
+++ js/1-document-ready.js
@@ -0,0 +1,7 @@
+'use strict';
+
+$(function(){
+    
+    // alert('test');
+    $('#first').css({color: 'red'}).hide(2000).show(3000);
+});
---
+++ js/2-selektory.js
@@ -0,0 +1,9 @@
+'use strict';
+var paragraphs = $('p');
+console.log(paragraphs);
+
+var paragraphOne = $('#paragraf');
+console.log(paragraphOne);
+
+var paragaphTwo = $('.paragraf');
+console.log(paragaphTwo);
